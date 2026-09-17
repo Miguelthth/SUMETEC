cb29ca7a0861d13b735d115368aa992212501f4c
Miguelthth <miguelth9316@gmail.com>
2026-09-16 19:07:36 -0700

    s

--- COTIZACION-REMISION/version.js
+++ COTIZACION-REMISION/version.js
@@ -1,3 +1,3 @@
 // Generado por build_deploy.py -- no editar.
-const VERSION_DEPLOY = '2026-09-16T23:31:29Z';
-const VERSION_CODIGO = '238a86f5c3';
+const VERSION_DEPLOY = '2026-09-17T02:06:05Z';
+const VERSION_CODIGO = '3569ff99f7';
